89a98a22ca8e3d1c8543f5468b0a8a53301434c8
MrArtemSid <artem.sid.key@gmail.com>
2022-01-03 00:22:54 +0400

    sdm845-common: Update display configuration
    Change-Id: I28bc14f49c980abeba79c15fd8a5eb998f989e8f

--- sdm845.mk
+++ sdm845.mk
@@ -148,7 +148,6 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.display.composer-service \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
@@ -156,14 +155,24 @@ PRODUCT_PACKAGES += \
     gralloc.sdm845 \
     hwcomposer.sdm845 \
     libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
     libqdMetaData \
+    libsdmcore \
+    libsdmutils \
     libtinyxml \
     libtinyxml.vendor \
+    libvulkan \
     memtrack.sdm845 \
-    vendor.display.config@1.0.vendor \
+    vendor.display.config@1.5 \
+    vendor.display.config@1.11.vendor \
     vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor \
     vendor.qti.hardware.display.allocator-service \
-    vendor.qti.hardware.display.mapper@2.0.vendor
+    vendor.qti.hardware.display.composer-service \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor \
+    vendor.qti.hardware.display.mapper@4.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
@@ -308,10 +317,6 @@ PRODUCT_PACKAGES += \
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_xiaomi
-
-# RenderScript
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
